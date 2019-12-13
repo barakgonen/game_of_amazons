a899f8829e877f231189aea8eadfbe503fe8f488
Barak Gonen <barakgonen9@gmail.com>
2019-12-13 11:31:05 +0200

    Added turn validator

--- server/turn_validator.py
+++ server/turn_validator.py
@@ -1,10 +1,7 @@
 import random
 
 class TurnValidator:
-    def __init__(self, game_board):
-        self.game_board = game_board
-
-    def is_step_valid(self, move):
+    def is_step_valid(self, move, game_board):
         val = random.randrange(0, 100000000, 1) # this should be with the board according to amazons game rules
         if (val % 2 == 0):
             print "is_step_valid, step is valid"
@@ -13,8 +10,8 @@ class TurnValidator:
             print "step_is_invalid"
             return False
     
-    def validate_move(self, move):
-        if (self.is_step_valid(move)):
+    def validate_move(self, move, board_game):
+        if (self.is_step_valid(move, board_game)):
           return True
         else:
             print "Step is invalid"
