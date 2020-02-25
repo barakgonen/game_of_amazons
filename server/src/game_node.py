3a02574ff5e54acd8e12c68765eeee4fb06e907f
Barak Gonen <barakgonen9@gmail.com>
2020-02-25 12:34:23 +0200

    Added random heuristic property for game node / game state

--- server/src/game_node.py
+++ server/src/game_node.py
@@ -12,11 +12,15 @@ class GameNode:
         self.black_amazons_pos = copy.deepcopy(black_amazons_pos)
         self.blocking_lst = copy.deepcopy(blocking_lst)
         self.available_steps_manager = available_steps_manager
+        self.random_heuristic_val = self.calculate_random_heuristic()
         # self.black_moves = len(available_steps_manager.get_available_states_for_player(board_size, self.white_amazons_pos, self.black_amazons_pos, self.blocking_lst, "BLACK"))
         # self.white_moves = len(available_steps_manager.get_available_states_for_player(board_size, self.white_amazons_pos, self.black_amazons_pos, self.blocking_lst, "WHITE"))
 
         ### TODO: CALCULATE HEURISTICS - RANDOM HEIURISTIC, MOVE-COUNT HEIURISTIC & MOBILITY HEIURISTIC
         return
+
+    def calculate_random_heuristic(self):
+        return random.randrange(1, 10, 1)
         
     def addChildNode(self, node):
         self.children.append(node)
